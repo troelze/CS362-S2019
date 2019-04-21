1c11faede2921879ae655a0270fe90b0dd8b0b97
jd-osu <dimedioj@oregonstate.edu>
2019-04-21 14:08:27 -0400

    Bug fix

--- projects/dimedioj/dominion/dominion.c
+++ projects/dimedioj/dominion/dominion.c
@@ -776,7 +776,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   int nextPlayer = currentPlayer + 1;
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
-  int z = 0;// this is the counter for the temp hand
+
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
   }
